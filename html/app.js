94ae09c520b25b78938826c829814cb3f2bf50f9
Fabian Gremke <fabian.gremke@dmi.de>
2021-03-11 00:40:28 +0100

    Add basic user-creation
    Add classes for user-management to js.
    Add dummy-creation to js.
    Add styling to css.

--- html/app.js
+++ html/app.js
@@ -1,3 +1,58 @@
+class SplitterEntry {
+  constructor (name) {
+    this.name = name;
+    this.guiElm = null;
+    this.splits_rest = true;
+    this.personal_slice = [];
+  }
+  addGui () {
+    if(this.guiElm == null) {
+      this.guiElm = Gui.newFriendListItem(this.name);
+    }
+    this.guiElm.querySelector("div.splitAmount").onclick=this.editAmount;
+    document.querySelector("#friendsListContainer > ul.friendslist").appendChild(this.guiElm);
+  }
+  editAmount () {
+    console.log("Clicked Amount-display for user "+name);
+  }
+}
+
+class SplitterColl {
+  constructor () {
+    this.splitters = [];
+  }
+  add (name) {
+    const newSplitter = new SplitterEntry(name);
+    this.splitters.push(newSplitter);
+    newSplitter.addGui();
+  }
+}
+
+const splitters = new SplitterColl();
+
+class Gui {
+  static newFriendListItem (friendName) {
+    const newLI = document.createElement("li");
+    newLI.classList.add("friendsListItem");
+
+    const newName = document.createElement("div");
+    newName.appendChild(document.createTextNode(friendName));
+    newName.classList.add("friendName");
+
+    const newSplitBox = document.createElement("div");
+    newSplitBox.classList.add("splitAmountBox");
+    const newSplitAmount = document.createElement("div");
+    newSplitAmount.classList.add("splitAmount");
+    newSplitAmount.appendChild(document.createTextNode("0.00"));
+    newSplitBox.appendChild(newSplitAmount);
+    newSplitBox.appendChild(document.createTextNode("€"));
+
+    newLI.appendChild(newName);
+    newLI.appendChild(newSplitBox);
+    return newLI;
+  }
+}
+
 function addElement() {
   const newDiv = document.createElement("div");
   const newCont = document.createTextNode("Hi! This text was generated.");
@@ -14,10 +69,25 @@ function resetPage() {
   addElement();
 }
 
+
+function genID() {
+  let r4= () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return r4() + r4();
+}
+
+function dummySplitter() {
+  splitters.add(genID());
+}
+
 function init() {
   //add on-clicks
   document.getElementById("addElm").onclick=addElement;
   document.getElementById("resetPage").onclick=resetPage;
+  document.getElementById("addPeopleButton").onclick=dummySplitter;
   return;
 }
 
